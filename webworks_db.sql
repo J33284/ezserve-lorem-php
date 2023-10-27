714c780224ed84d9ddb2453983d2de139d1b9be0
webworks12345 <jirehsevein@gmail.com>
2023-10-27 10:31:57 +0800

    1

--- webworks_db.sql
+++ webworks_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 25, 2023 at 04:39 AM
+-- Generation Time: Oct 27, 2023 at 04:30 AM
 -- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,17 +29,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `adminID` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `usertype` varchar(100) NOT NULL
+  `usertype` varchar(30) NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`adminID`, `username`, `password`, `usertype`) VALUES
-(1, 'admin', 'admin12345', 'admin');
+INSERT INTO `admin` (`adminID`, `email`, `username`, `password`, `usertype`, `status`, `created`, `updated`) VALUES
+(1, 'bvrlisah@gmail.com', 'admin', '827ccb0eea8a706c4c34a16891f84e7b', 'admin', 1, '2023-10-26 21:01:34', '2023-10-26 21:01:34');
 
 -- --------------------------------------------------------
 
@@ -62,7 +66,7 @@ CREATE TABLE `branches` (
 
 CREATE TABLE `business` (
   `businessCode` int(11) NOT NULL,
-  `userID` int(11) NOT NULL,
+  `ownerID` int(11) NOT NULL,
   `ownerName` varchar(100) NOT NULL,
   `ownerAddress` varchar(100) NOT NULL,
   `busName` varchar(100) NOT NULL,
@@ -76,15 +80,75 @@ CREATE TABLE `business` (
   `phone` varchar(100) NOT NULL,
   `mobile` varchar(100) NOT NULL,
   `coordinates` varchar(100) NOT NULL,
-  `permits` mediumblob NOT NULL
+  `permits` mediumblob NOT NULL,
+  `status` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `business`
 --
 
-INSERT INTO `business` (`businessCode`, `userID`, `ownerName`, `ownerAddress`, `busName`, `busType`, `house_building`, `street`, `barangay`, `city_municipality`, `province`, `region`, `phone`, `mobile`, `coordinates`, `permits`) VALUES
-(30, 25, 'Alisah', 'sdsad', 'asasd', '1', 'sad', 'JEREOS', '', 'ILOILO CITY (CAPITAL)', 'ILOILO', '6', '+639203633104', '09452781051', '12121212', 0x636f72652076616c75657320616e642068796769656e6520616e642073616e69746174696f6e2e646f6378);
+INSERT INTO `business` (`businessCode`, `ownerID`, `ownerName`, `ownerAddress`, `busName`, `busType`, `house_building`, `street`, `barangay`, `city_municipality`, `province`, `region`, `phone`, `mobile`, `coordinates`, `permits`, `status`) VALUES
+(5, 2, 'Alisah Mae', 'Dayot Subdivision Jereos Street La Paz', 'Puga Funeral', '1', '12', 'JEREOS', 'Jereos', 'ILOILO CITY (CAPITAL)', 'ILOILO', '6', '09452781051', '09452781051', '10.71726102925748, 122.56800678334658', 0x6173736574732f75706c6f6164732f62616a616a2e706e67, 0),
+(6, 3, 'Jireh', 'Lapuz', 'Jireh\'s Catering', '2', '20', 'Lapuz', 'Alalasan', 'Iloilo City', 'Iloilo', 'IV', '3202918', '09203633104', '10.71726102925748', 0x6173736574732f75706c6f6164732f4b4147415741442e706e67, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `business_owner`
+--
+
+CREATE TABLE `business_owner` (
+  `ownerID` int(11) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `birthday` date NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `number` varchar(30) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `usertype` varchar(50) NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `business_owner`
+--
+
+INSERT INTO `business_owner` (`ownerID`, `fname`, `lname`, `birthday`, `email`, `number`, `username`, `password`, `usertype`, `status`, `created`, `updated`) VALUES
+(2, 'Alisah', 'Bolivar', '0001-01-07', 'bvrlisah@gmail.com', '09452781051', 'Owner', '827ccb0eea8a706c4c34a16891f84e7b', 'business owner', 1, '2023-10-26 09:03:03', '2023-10-26 09:03:03'),
+(3, 'Jireh', 'Nieves', '2001-02-13', 'jireh@gmail.com', '09203633104', 'Owner2', '827ccb0eea8a706c4c34a16891f84e7b', 'business owner', 1, '2023-10-27 00:45:59', '2023-10-27 00:45:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client`
+--
+
+CREATE TABLE `client` (
+  `clientID` int(11) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `birthday` date NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `number` varchar(30) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `usertype` varchar(50) NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `client`
+--
+
+INSERT INTO `client` (`clientID`, `fname`, `lname`, `birthday`, `email`, `number`, `username`, `password`, `usertype`, `status`, `created`, `updated`) VALUES
+(1, 'Jireh', 'NIevs', '2001-12-01', 'lalalaamb@gmail.com', '09452781051', 'jireh', '827ccb0eea8a706c4c34a16891f84e7b', 'client', 0, '2023-10-25 08:51:53', '2023-10-25 08:51:53'),
+(2, 'Jireh ', 'Nieves', '2001-12-02', 'jirehsevein@gmail.com', '09452781051', 'User', '827ccb0eea8a706c4c34a16891f84e7b', 'client', 1, '2023-10-25 08:59:20', '2023-10-25 08:59:20');
 
 -- --------------------------------------------------------
 
@@ -98,36 +162,6 @@ CREATE TABLE `email_verify` (
   `email` varchar(100) NOT NULL,
   `verification_code` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `userID` int(11) NOT NULL,
-  `fname` varchar(50) NOT NULL,
-  `lname` varchar(50) NOT NULL,
-  `birthday` date NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `number` varchar(30) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `usertype` varchar(50) NOT NULL,
-  `status` tinyint(4) NOT NULL DEFAULT 0,
-  `created` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`userID`, `fname`, `lname`, `birthday`, `email`, `number`, `username`, `password`, `usertype`, `status`, `created`, `updated`) VALUES
-(24, 'alisah', 'bolivar', '2001-07-01', 'bvrlisah@gmail.com', '09293475344', 'lorem', '827ccb0eea8a706c4c34a16891f84e7b', 'client', 1, '2023-10-20 03:30:44', '2023-10-20 03:30:44'),
-(25, 'mae', 'bolivar', '2001-07-02', 'lalalaamb@gmail.com', '0929347534', 'ipsum', '827ccb0eea8a706c4c34a16891f84e7b', 'business owner', 1, '2023-10-20 03:32:25', '2023-10-20 03:32:25'),
-(26, 'Jireh Antonette', 'Nieves', '2001-07-13', 'jireh@gmail.com', '09203633104', 'lily', '827ccb0eea8a706c4c34a16891f84e7b', 'client', 0, '2023-10-24 06:23:15', '2023-10-24 06:23:15');
 
 --
 -- Indexes for dumped tables
@@ -150,19 +184,19 @@ ALTER TABLE `branches`
 --
 ALTER TABLE `business`
   ADD PRIMARY KEY (`businessCode`),
-  ADD KEY `userID` (`userID`);
+  ADD KEY `ownerID` (`ownerID`);
 
 --
--- Indexes for table `email_verify`
+-- Indexes for table `business_owner`
 --
-ALTER TABLE `email_verify`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `business_owner`
+  ADD PRIMARY KEY (`ownerID`);
 
 --
--- Indexes for table `users`
+-- Indexes for table `client`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`userID`);
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`clientID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -175,28 +209,22 @@ ALTER TABLE `admin`
   MODIFY `adminID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `branches`
---
-ALTER TABLE `branches`
-  MODIFY `branchCode` int(50) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `business`
 --
 ALTER TABLE `business`
-  MODIFY `businessCode` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `businessCode` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `email_verify`
+-- AUTO_INCREMENT for table `business_owner`
 --
-ALTER TABLE `email_verify`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `business_owner`
+  MODIFY `ownerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `client`
 --
-ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+ALTER TABLE `client`
+  MODIFY `clientID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -206,7 +234,7 @@ ALTER TABLE `users`
 -- Constraints for table `business`
 --
 ALTER TABLE `business`
-  ADD CONSTRAINT `userID` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`);
+  ADD CONSTRAINT `ownerID` FOREIGN KEY (`ownerID`) REFERENCES `business_owner` (`ownerID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
