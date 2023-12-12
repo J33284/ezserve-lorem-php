d2825d79ced9e8d3f37f35c56a723391a4d1caab
webworks12345 <jirehsevein@gmail.com>
2023-12-12 14:45:51 +0800

    ...

--- elements/client-side-nav.php
+++ elements/client-side-nav.php
@@ -1,7 +1,6 @@
 <div class="side-nav d-lg-block collapse">
         <div class="">
           <div class="nav-item"><a href="<?php echo SITE_URL ?>/?page=client_profile" class="py-2"><i class="bi bi-person"></i><span>My Profile</span></i></a></div>
-          <div class="nav-item"><a href="<?php echo SITE_URL ?>/?page=client_purchase"><i class="bi bi-bag"></i><span>Order History</span></i></a></div>
-          <div class="nav-item"><a href="<?php echo SITE_URL ?>/?page=client_voucher" class="py-2"><i class="bi bi-tags"></i><span>My Vouchers</span></i></a></div>
-        </div>
+          <div class="nav-item"><a href="<?php echo SITE_URL ?>/?page=client-order-history"><i class="bi bi-bag"></i><span>Order History</span></i></a></div>
+           </div>
       </div>
